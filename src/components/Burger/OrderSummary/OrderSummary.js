7cc86ab1bf4dd0bab00f9ef4ffb5a0273d04c615
kozmobot <kozmobot@gmail.com>
2018-01-11 23:01:09 +0300

    show/hide modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,7 +16,7 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p>Continue to checkout</p>
+      <p>Continue to Checkout?</p>
     </Aux>
   );
 };
