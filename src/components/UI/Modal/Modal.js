bf17a3a1825e20f715c6374c0cd00f9369901509
kozmobot <kozmobot@gmail.com>
2018-01-14 21:15:03 +0300

    update folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxy';
+import Aux from '../../../hoc/Aux/Auxy';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
